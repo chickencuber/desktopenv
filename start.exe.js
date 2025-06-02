a1699cde3e7843d832fd2b2aa6380ae0719bbda0
chickencuber <n1batman728@gmail.com>
2025-06-01 19:13:42 -0500

    added old terminal emulator

--- start.exe.js
+++ start.exe.js
@@ -1,5 +1,7 @@
 if(!await FS.exists("/user/desktop/game_best")) await FS.addFile("/user/desktop/game_best", "0");
-if(!await FS.exists("/user/desktop/wallpaper.png")) await FS.copy("/bin/desktop/wallpapers/default.png", "/user/desktop/wallpaper.png")
-if(!await FS.exists("/user/desktop/apps")) await FS.copy("/bin/desktop/apps", "/user/desktop/apps")
-if(!await FS.exists("/user/desktop/24hour")) await FS.addFile("/user/desktop/24hour", "0")
+if(!await FS.exists("/user/desktop/wallpaper.png")) await FS.copy("/bin/desktop/wallpapers/default.png", "/user/desktop/wallpaper.png");
+if(!await FS.exists("/user/desktop/apps")) await FS.copy("/bin/desktop/apps", "/user/desktop/apps");
+if(!await FS.exists("/user/desktop/24hour")) await FS.addFile("/user/desktop/24hour", "0");
+if(!await FS.exists("/user/desktop/terminal")) await FS.addDir("/user/desktop/terminal");
+if(!await FS.exists("/user/desktop/terminal/.startup.sh")) await FS.copy("/bin/desktop/terminal/.startup.sh", "/user/desktop/terminal/.startup.sh");
 
